693584c36c48a8b613018ff035bfee57e2a73230
Kamal Kishore Bochiwal <108949333+kamalkhandal23@users.noreply.github.com>
2025-02-20 11:49:30 +0000

    conversion of values

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = false;
+let valueInNumber = Number(score); //it will convert the value in number
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber); //NaN
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+let isLoggedIn = 1;
+let valueInBool = Boolean(isLoggedIn);
+console.log(valueInBool);
+
+/*
+0 => false
+1 => true
+"" => false
+"kamal" => true
+*/
+
+
+
