59cdeb8c1f219caf63cbfdc2473507d741f0f4f4
Kamal Kishore Bochiwal <108949333+kamalkhandal23@users.noreply.github.com>
2025-02-20 11:36:08 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //it means that we will use the newer version of js
+
+//alert(3+3); //it will show error bcz we are at node not at browser
+
+//primitive datatypes
+/*
+1. number => 2^53 is a range
+2. bigint => for larger values
+3. string => ""
+4. boolean => true/false
+5. null => standalone value
+6. undefined => value not given yet
+7. symbol => uniqueness
+*/
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
+//non-primitive datatypes
+//object
+
+
